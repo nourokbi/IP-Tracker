b94e35552d2568c907f948b150393484c0c7ce95
Nour Okbi <73375144+nourokbi@users.noreply.github.com>
2025-08-29 00:30:23 +0300

    revert validation change

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,30 @@ function App() {
   const API_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=at_PibClQ7PRQ3IQmJ165EpSCUreuaMg&ipAddress=${
     ipAddress ? ipAddress : ""
   }`;
-  function validateAddress(address) {
-    // Basic IP address validation regex
-    const ipRegex =
-      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    const domainRegex =
-      /^(?!-)(?:[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,6}$/;
-    if (!ipRegex.test(address) && !domainRegex.test(address)) {
-      setIpAddress("");
-      document.querySelector(".no-data").classList.add("active");
-      document.querySelector(".no-data").textContent =
-        "Please enter a valid IP address or domain";
-      // alert("Please enter a valid IP address or domain");
-    }
-  }
+  // function validateAddress(address) {
+  //   // Basic IP address validation regex
+  //   const ipRegex =
+  //     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  //   const domainRegex =
+  //     /^(?!-)(?:[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,6}$/;
+  //   if (!ipRegex.test(address) && !domainRegex.test(address)) {
+  //     setIpAddress("");
+  //     document.querySelector(".no-data").classList.add("active");
+  //     document.querySelector(".no-data").textContent =
+  //       "Please enter a valid IP address or domain";
+  //     // alert("Please enter a valid IP address or domain");
+  //   }
+  // }
 
   function handleSearch(e) {
     e.preventDefault();
     // Fetch location data from the API
-    if (ipAddress !== "" && validateAddress(ipAddress)) {
-      fetch(API_URL)
-        .then((response) => response.json())
-        .then((data) => {
-          setLocationData(data);
-        });
-    }
+    // if (ipAddress !== "" && validateAddress(ipAddress)) {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setLocationData(data);
+      });
   }
 
   return (
