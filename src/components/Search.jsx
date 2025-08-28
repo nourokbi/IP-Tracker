4a930cb90fe997c3e00f9432f110cd23248d216e
Nour Okbi <73375144+nourokbi@users.noreply.github.com>
2025-08-28 16:42:31 +0300

    adding project files

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function Search({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+export default Search;
